69f26a61aa00f4968ce79bd97f4e280636fb67d8
CaliforniaDemise <vwwwwv@proton.me>
2024-09-01 22:47:12 +0300

    Move ModelTrident from ../model to ../model.entity

--- src/main/java/surreal/backportium/client/model/entity/ModelTrident.java
+++ src/main/java/surreal/backportium/client/model/entity/ModelTrident.java
@@ -1,4 +1,4 @@
-package surreal.backportium.client.model;
+package surreal.backportium.client.model.entity;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
