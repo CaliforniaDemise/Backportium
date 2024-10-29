8b95e56c1e04e65d362bcab087ad2a3bc8253b73
CaliforniaDemise <vwwwwv@proton.me>
2024-10-29 17:42:50 +0300

    Fix some debarked logs not showing on creative tab.

--- src/main/java/surreal/backportium/item/v1_13/ItemBlockDebarkedLog.java
+++ src/main/java/surreal/backportium/item/v1_13/ItemBlockDebarkedLog.java
@@ -1,13 +1,16 @@
 package surreal.backportium.item.v1_13;
 
 import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.translation.I18n;
 import surreal.backportium.util.RandomHelper;
 
 import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 public class ItemBlockDebarkedLog extends ItemBlock {
 
@@ -17,6 +20,29 @@ public class ItemBlockDebarkedLog extends ItemBlock {
         super(block);
         this.origLog = origLog;
         this.setTranslationKey(origLog.getTranslationKey());
+    }
+
+    @Override
+    public CreativeTabs getCreativeTab() {
+        return this.getOrigItem().getCreativeTab();
+    }
+
+    @Nonnull
+    @Override
+    public CreativeTabs[] getCreativeTabs() {
+        return this.getOrigItem().getCreativeTabs();
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
+        NonNullList<ItemStack> ass = NonNullList.create();
+        this.getOrigItem().getSubItems(tab, ass);
+        ass.forEach(stack -> {
+            ItemStack s = new ItemStack(this, 1, stack.getMetadata());
+            s.setTagCompound(stack.getTagCompound());
+            items.add(s);
+        });
     }
 
     @Override
