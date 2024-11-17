d6508f9e774ec5dcb714a79ffd34eea1eba6dff1
CaliforniaDemise <vwwwwv@proton.me>
2024-11-17 21:12:26 +0300

    Add Deep frozen ocean.

--- src/main/java/surreal/backportium/world/biome/ModBiomes.java
+++ src/main/java/surreal/backportium/world/biome/ModBiomes.java
@@ -18,6 +18,7 @@ public class ModBiomes {
     private static final List<Biome> BIOMES = new ArrayList<>();
 
     public static final BiomeOceanFrozen FROZEN_OCEAN = register(new BiomeOceanFrozen(new Biome.BiomeProperties("Frozen Ocean 2").setBaseHeight(-1.0F).setHeightVariation(0.1F).setTemperature(0.0F).setRainfall(0.5F).setSnowEnabled()), "frozen_ocean");
+    public static final BiomeOceanFrozen DEEP_FROZEN_OCEAN = register(new BiomeOceanFrozen(new Biome.BiomeProperties("Deep Frozen Ocean").setBaseHeight(-1.8F).setHeightVariation(0.1F).setRainfall(0.5F).setSnowEnabled()), "deep_frozen_ocean");
 
     public static <T extends Biome> T register(T biome, String name) {
         biome.setRegistryName(new ResourceLocation(Tags.MOD_ID, name));
@@ -30,7 +31,10 @@ public class ModBiomes {
         BIOMES.forEach(registry::register);
 
         BiomeManager.addBiome(BiomeManager.BiomeType.ICY, new BiomeManager.BiomeEntry(FROZEN_OCEAN, 10));
+        BiomeManager.addBiome(BiomeManager.BiomeType.ICY, new BiomeManager.BiomeEntry(DEEP_FROZEN_OCEAN, 10));
         BiomeManager.oceanBiomes.add(FROZEN_OCEAN);
+        BiomeManager.oceanBiomes.add(DEEP_FROZEN_OCEAN);
         BiomeDictionary.addTypes(FROZEN_OCEAN, COLD, OCEAN, SNOWY);
+        BiomeDictionary.addTypes(DEEP_FROZEN_OCEAN, COLD, OCEAN, SNOWY);
     }
 }
